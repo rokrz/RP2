86a939f65720b33f9119cb21079e63434395c9e7
rokrz <41301000+rokrz@users.noreply.github.com>
2021-09-30 03:32:49 -0300

    Animacao de botoes/melhoria menu

---
+++ Assets/Scripts/ButtonVisualUpdate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonVisualUpdate : MonoBehaviour
+{
+    [SerializeField]
+    private Text buttonText;
+    public void setTextColorOn()
+    {
+        buttonText.color = new Color(0.02f,0.89f,0.05f);
+    }
+    public void setTextColorOff()
+    {
+        buttonText.color = new Color(0.14f, 0.45f, 0.08f);
+    }
+}
