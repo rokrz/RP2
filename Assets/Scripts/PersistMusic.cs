2879eee4677bb56d70c84973548357142aac9c13
seijiBellocchi <56082379+seijiBellocchi@users.noreply.github.com>
2021-10-04 02:00:42 -0300

    BGM persistente

--- Assets/Scripts/PersistMusic.cs
+++ Assets/Scripts/PersistMusic.cs
@@ -4,11 +4,38 @@ using UnityEngine;
 
 public class PersistMusic : MonoBehaviour
 {
+    private void Start()
+    {
+
+    }
+    //play global
+    private static PersistMusic instance = null;
+    public static PersistMusic Instance
+    {
+        get { return instance; }
+    }
     private void Awake()
     {
-        GameObject[] objs = GameObject.FindGameObjectsWithTag("music");
-        if (objs.Length > 1)
-           Destroy(this.gameObject);
+        //GameObject[] objs = GameObject.FindGameObjectsWithTag("music");
+        //if (objs.Length > 1)
+        //   Destroy(this.gameObject);
+        //DontDestroyOnLoad(this.gameObject);
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+
         DontDestroyOnLoad(this.gameObject);
+
+    }
+
+    private void Update()
+    {
+
     }
 }
