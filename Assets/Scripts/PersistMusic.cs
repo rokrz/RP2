a7203ca4e3fe10a9b434bb28f482a72e44567fef
seijiBellocchi <56082379+seijiBellocchi@users.noreply.github.com>
2021-10-05 01:45:48 -0300

    BGM persistente de verdade
    Agora foi

--- Assets/Scripts/PersistMusic.cs
+++ Assets/Scripts/PersistMusic.cs
@@ -16,7 +16,7 @@ public class PersistMusic : MonoBehaviour
     }
     private void Awake()
     {
-        //GameObject[] objs = GameObject.FindGameObjectsWithTag("music");
+        //GameObject[] objs = GameObject.FindGameObjectsWithTag("music
         //if (objs.Length > 1)
         //   Destroy(this.gameObject);
         //DontDestroyOnLoad(this.gameObject);
