815151dc4f2e99d29d94e271b9aff159d629a82e
seijiBellocchi <56082379+seijiBellocchi@users.noreply.github.com>
2021-11-10 14:37:43 -0300

    WIP levelselector
    grid de botões adicionados, não-interagível ainda (?)

--- Assets/Scripts/LevelSelector.cs
+++ Assets/Scripts/LevelSelector.cs
@@ -1,51 +1,133 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.EventSystems;
+
+
+public class PageSwiper : MonoBehaviour, IDragHandler, IEndDragHandler
+{
+    private Vector3 panelLocation;
+    public float percentThreshold = 0.2f;
+    public float easing = 0.5f;
+    public int totalPages = 1;
+    private int currentPage = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        panelLocation = transform.position;
+    }
+    public void OnDrag(PointerEventData data)
+    {
+        float difference = data.pressPosition.x - data.position.x;
+        transform.position = panelLocation - new Vector3(difference, 0, 0);
+    }
+    public void OnEndDrag(PointerEventData data)
+    {
+        float percentage = (data.pressPosition.x - data.position.x) / Screen.width;
+        if (Mathf.Abs(percentage) >= percentThreshold)
+        {
+            Vector3 newLocation = panelLocation;
+            if (percentage > 0 && currentPage < totalPages)
+            {
+                currentPage++;
+                newLocation += new Vector3(-Screen.width, 0, 0);
+            }
+            else if (percentage < 0 && currentPage > 1)
+            {
+                currentPage--;
+                newLocation += new Vector3(Screen.width, 0, 0);
+            }
+            StartCoroutine(SmoothMove(transform.position, newLocation, easing));
+            panelLocation = newLocation;
+        }
+        else
+        {
+            StartCoroutine(SmoothMove(transform.position, panelLocation, easing));
+        }
+    }
+    IEnumerator SmoothMove(Vector3 startpos, Vector3 endpos, float seconds)
+    {
+        float t = 0f;
+        while (t <= 1.0)
+        {
+            t += Time.deltaTime / seconds;
+            transform.position = Vector3.Lerp(startpos, endpos, Mathf.SmoothStep(0f, 1f, t));
+            yield return null;
+        }
+    }
+}
+
+
 
 public class LevelSelector : MonoBehaviour
 {
-    public GameObject levelButton;
-    int currentPage = 0;
-    private Dictionary<string, List<LevelCreator>> worldHolder;
-    private string[] worlds;
+    public GameObject levelHolder; // panel
+    public GameObject levelIcon; //button prefab
+    public GameObject thisCanvas;
+    public int numberOfLevels = 50; // number of levels
+    public Vector2 iconSpacing; 
+    private Rect panelDimensions;
+    private Rect iconDimensions;
+    private int amountPerPage;
+    private int currentLevelCount;
 
-    private void Start()
+    // Start is called before the first frame update
+    void Start()
     {
-        worldHolder = GridMaker.instance.worldHolder;
-        worlds = new string[worldHolder.Keys.Count];
-        worldHolder.Keys.CopyTo(worlds,0);
-        LoadLevelsPerPage();
+        panelDimensions = levelHolder.GetComponent<RectTransform>().rect;
+        iconDimensions = levelIcon.GetComponent<RectTransform>().rect;
+        int maxInARow = Mathf.FloorToInt((panelDimensions.width + iconSpacing.x) / (iconDimensions.width + iconSpacing.x));
+        int maxInACol = Mathf.FloorToInt((panelDimensions.height + iconSpacing.y) / (iconDimensions.height + iconSpacing.y));
+        amountPerPage = maxInARow * maxInACol;
+        Debug.Log(amountPerPage);
+        int totalPages = Mathf.CeilToInt((float)numberOfLevels / amountPerPage);
+        LoadPanels(totalPages);
     }
-
-    void NextPage()
+    void LoadPanels(int numberOfPanels)
     {
-        currentPage++;
-        LoadLevelsPerPage();
-    }
+        GameObject panelClone = Instantiate(levelHolder) as GameObject;
+        PageSwiper swiper = levelHolder.AddComponent<PageSwiper>();
+        swiper.totalPages = numberOfPanels;
 
-    void PreviousPage() {
-        currentPage--;
-        LoadLevelsPerPage();
-    }
-
-    void LoadLevelsPerPage()
-    {
-        int level = 0;
-        if (currentPage>=0 && currentPage<worlds.Length)
+        for (int i = 1; i <= numberOfPanels; i++)
         {
-            foreach (LevelCreator lc in worldHolder[worlds[currentPage]])
-            {
-                GameObject auxObject = Instantiate(levelButton, new Vector3(0.0f, 0.0f), Quaternion.identity);
-                auxObject.GetComponent<LevelSelectorButton>().SetButtonValues(currentPage, level);
-                level++;
-            }
+            GameObject panel = Instantiate(panelClone) as GameObject;
+            panel.transform.SetParent(thisCanvas.transform, false);
+            panel.transform.SetParent(levelHolder.transform);
+            panel.name = "Page-" + i;
+            panel.GetComponent<RectTransform>().localPosition = new Vector2(panelDimensions.width * (i - 1), 0);
+            SetUpGrid(panel);
+            int numberOfIcons = i == numberOfPanels ? numberOfLevels - currentLevelCount : amountPerPage;
+            LoadIcons(numberOfIcons, panel);
+        }
+        Destroy(panelClone);
+    }
+    void SetUpGrid(GameObject panel)
+    {
+        GridLayoutGroup grid = panel.AddComponent<GridLayoutGroup>();
+        grid.cellSize = new Vector2(iconDimensions.width, iconDimensions.height);
+        grid.childAlignment = TextAnchor.MiddleCenter;
+        grid.spacing = iconSpacing;
+    }
+    void LoadIcons(int numberOfIcons, GameObject parentObject)
+    {
+        for (int i = 1; i <= numberOfIcons; i++)
+        {
+            currentLevelCount++;
+            GameObject icon = Instantiate(levelIcon) as GameObject;
+            icon.transform.SetParent(thisCanvas.transform, false);
+            icon.transform.SetParent(parentObject.transform);
+            icon.name = "Level " + i;
+            icon.GetComponentInChildren<Text>().text=("Level " + currentLevelCount);
         }
     }
 
-    public void ReturnToMainMenu()
+    // Update is called once per frame
+    void Update()
     {
-        SceneManager.LoadScene("Menu");
-    }
 
+    }
 }
